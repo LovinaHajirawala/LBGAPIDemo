d0fe7de8f9a886608930f0a3d82fb0a54b21a5a3
Lovina Vijay Hajirawala <vhajirawala@gmail.com>
2025-06-18 11:03:33 +0530

    Initial Commit

---
+++ LBGAPIDemoTests/LBGAPIDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LBGAPIDemoTests.swift
+//  LBGAPIDemoTests
+//
+//  Created by Lovina v. Hajirawala on 18/06/25.
+//
+
+import Testing
+@testable import LBGAPIDemo
+
+struct LBGAPIDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
